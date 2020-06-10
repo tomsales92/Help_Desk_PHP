76e802887e92d5608770ee8c175a718ab9a2f6ae
Everton Sales <46347115+tomsales92@users.noreply.github.com>
2020-06-10 19:31:08 -0300

    Initial Project

---
+++ valida_login.php
@@ -0,0 +1,13 @@
+<?php
+ /*  print_r($_GET);
+  echo '<br />';
+  echo $_GET['email'];
+  echo '<br />';
+  echo $_GET['password']; */
+
+  print_r($_POST);
+  echo '<br />';
+  echo $_POST['email'];
+  echo '<br />';
+  echo $_POST['password'];
+?>
